5c3c1e2a831bfe79a1f663111845b0253db94145
France Samaniego <france.samaniego@oddeci.com>
2026-04-16 09:17:30 +0800

    Export Dashboard

---
+++ wwwroot/js/pdf-export.js
@@ -0,0 +1,9 @@
+﻿window.downloadFile = (fileName, contentType, byteArray) => {
+    const blob = new Blob([byteArray], { type: contentType });
+    const url = URL.createObjectURL(blob);
+    const anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.download = fileName;
+    anchor.click();
+    URL.revokeObjectURL(url);
+};
